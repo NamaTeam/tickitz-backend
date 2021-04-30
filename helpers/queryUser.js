7c8d4c535119fdaca5b27e27be24a5a8a194beb4
DendyAR <anugerah700@gmail.com>
2021-04-30 16:58:22 +0700

    add user

---
+++ helpers/queryUser.js
@@ -0,0 +1,36 @@
+const { request } = require("express")
+
+const queryUser = {
+    getAllUsers: ()=>{
+        const query = `SELECT id,email,username,first_name,last_name,phone,photo FROM users`;
+        return query
+    },
+
+    getUserById:(request)=>{
+        const id = request
+        console.log(id,"ini id")
+        const getUserbyid=`SELECT id,email,username,first_name,last_name,phone,photo FROM users WHERE id=${parseInt(id)}`
+    
+    
+        return getUserbyid
+    },
+
+    addNewUser:(request)=>{
+        const {email} = request
+        const addNewUsers=`SELECT * FROM users WHERE email='${email}'`
+        
+        return addNewUsers
+
+
+    },
+
+    deleteUserById:(request)=>{
+        const {id} = request
+        const deleteUserById=`DELETE FROM users WHERE id='${id}'`
+
+        return deleteUserById
+    }
+    
+}
+
+module.exports=queryUser
