26c2f8c39b0cb91a2ae3e5ea93a4d011d0bbc94c
rpetzandrian <rpetzandrian@gmail.com>
2021-05-05 12:28:43 +0700

    get all movies fixing bug

--- routes/Movies.js
+++ routes/Movies.js
@@ -4,11 +4,11 @@ const { uploadPoster } = require('../helpers/fromUpload');
 const moviesModel = require('../models/Movies');
 
 route.post('/', uploadPoster, moviesController.addMovies);
-route.get('/', moviesController.getMoviesNow);
+route.get("/", moviesController.getAllMovies)
+route.get('/date', moviesController.getMoviesNow);
 route.get('/month', moviesController.getMoviesByMonth)
 route.get("/:id", moviesController.getMovies)
 route.delete("/:id", moviesController.deleteMovies)
-route.get("/", moviesController.getAllMovies)
 route.patch('/:id', uploadPoster, moviesController.updateMovies);
 
 module.exports = route
