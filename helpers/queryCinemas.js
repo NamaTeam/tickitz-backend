419cee05a36eff383bae0bdc66e31c69cf96833b
rpetzandrian <rpetzandrian@gmail.com>
2021-05-08 09:20:19 +0700

    fix bugs - set to default

--- helpers/queryCinemas.js
+++ helpers/queryCinemas.js
@@ -1,10 +1,10 @@
+const { request } = require("express");
+
 const queryCinemas = {
   showCinemas: (request) => {
     const query = `SELECT a.id, a.name, a.city, a.street, a.street_number, a.created_at, a.updated_at, a.logo, b.id as schedule_id, b.movie_id, b.start_date, b.price, b.start_time
-                  FROM cinemas as a
-                  INNER JOIN (SELECT a.id, a.movie_id, a.start_date, a.price, a.cinema_id, a.start_time from schedule as a) as b on b.cinema_id = a.id 
-                  WHERE a.city = '${request.city.toLowerCase()}'`
-
+	FROM cinemas as a
+	INNER JOIN (SELECT a.id, a.movie_id, a.start_date, a.price, a.cinema_id, a.start_time from schedule as a) as b on b.cinema_id = a.id WHERE a.city = '${request.toLowerCase()}'`
     return query;
   },
 
@@ -15,11 +15,8 @@ const queryCinemas = {
     return query;
   },
 
-  getAllCinemas: (request) => {
-    const getallCinemas = (`SELECT * FROM cinemas 
-                  WHERE city = '${request.city.toLowerCase()}'
-                  ORDER BY created_at DESC
-                  LIMIT '${request.limit || 10}' OFFSET '${(request.page - 1) * request.limit || 0}'`)
+  getAllCinemas: () => {
+    const getallCinemas = (`SELECT * FROM cinemas `)
     return getallCinemas
   },
 
