4d3fb68411e8909ded45a91a109a9db993d0b6ae
deaedria <dea.edria@gmail.com>
2021-09-01 23:31:23 +0700

    merge index.js

--- index.js
+++ index.js
@@ -3,32 +3,34 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
-const cors = require('cors');
-app.use(express.static('public'));
-
+const cors = require("cors");
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-let whitelist = ['http://localhost:3000','http://54.205.45.185:3000']
+let whitelist = [
+  "http://localhost:3000",
+  "http://54.205.45.185:3000",
+  "http://localhost:6001",
+  "http://tickitz.mooo.com",
+  "https://tickitz.mooo.com",
+  "http://tickitz.rpetz.my.id",
+  "https://tickitz.rpetz.my.id",
+];
 let corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
+      callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error("Not allowed by CORS"));
     }
-  }
-}
-
+  },
+};
 app.use(cors(corsOptions));
-
 const router = require("./routes");
 router(app, "/tickitz/api");
-
 app.get("*", (req, res) => {
   res.send("Not Found!");
 });
-
 app.listen(port, () => {
   console.log(
     `app listening at http://${process.env.HOST || "localhost"}:${port}`
